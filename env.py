5a1f65ad6d7ab90b118b8de78735f86f912f65ee
Andrea <ajcawley1@gmail.com>
2025-09-01 13:52:25 +0100

    install database URL

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+# Set your database URL here
+os.environ['DATABASE_URL'] = 'postgresql://neondb_owner:npg_Uupor3x7eWQK@ep-lively-salad-a277p3ej.eu-central-1.aws.neon.tech/half_wish_grave_860921'
